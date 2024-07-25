e1d981867ce25120b9bb113d8191fb8207d7e147
JohnCudera214 <jcjohncudera@gmail.com>
2024-07-25 16:57:41 +0800

    Add project files.

---
+++ Number-Word/Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Word
+{
+    internal class Converter
+    {
+        public static readonly string[] Units = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        public static readonly string[] Teens = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        public static readonly string[] Tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        public static readonly string[] Thousands = { "", "Thousand", "Million", "Billion" };
+
+        public static string Convert(int number)
+        {
+
+            if (number == 0) return "Zero";
+
+            int i = 0;
+            string words = "";
+
+            while (number > 0)
+            {
+                if (number % 1000 != 0)
+                {
+                    words = Helper(number % 1000) + " " + Thousands[i] + " " + words;
+                }
+                number /= 1000;
+                i++;
+            }
+
+            return words.Trim();
+        }
+
+        private static string Helper(int number)
+        {
+            if (number == 0) return "";
+
+            if (number < 10) return Units[number];
+            if (number < 20) return Teens[number - 10];
+            if (number < 100) return Tens[number / 10] + " " + Helper(number % 10);
+
+            return Units[number / 100] + " Hundred" + (number % 100 > 0 ? " " + Helper(number % 100) : "");
+        }
+    }
+}
---
+++ Number-Word/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static Number_Word.Converter;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Number_Word
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        int word;
+        private void btn01_Click(object sender, RoutedEventArgs e)
+        {   
+            //textblock01.Text = Converter.Convert(int.Parse(textbox1.Text));
+            if (textbox1.Text == "")
+            {
+                textblock_02.Text = "NOTE: PLEASE ENTER A WHOLE NUMBER!";
+            }
+            else
+            {
+                word = int.Parse(textbox1.Text);
+                textbox6.Text = Converter.Convert(int.Parse(textbox1.Text));
+                textblock_02.Text = "";
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textbox1.Text == "")
+            {
+                textblock_02.Text = "NOTE: PLEASE ENTER A WHOLE NUMBER!";
+            }
+            else
+            {
+                textblock_02.Text = "";
+                textbox1.Text = "";
+                textbox6.Text = "";
+            }
+        }
+    }
+}
